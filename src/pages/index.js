d3a24c250bf58b1e9fc048676063954a4bac0fbc
an-lee <an.lee.work@gmail.com>
2021-12-26 08:31:43 +0800

    hero button

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg margin-horiz--sm"
             to="/api/intro">
-            Developer Docs
+            API Docs
           </Link>
         </div>
       </div>
